00ff771ae9a1320c1d8ef853415174e156cd0b59
Thabo <thabochambule1@gmail.com>
2024-08-23 14:41:38 +0200

    add buttons and dialogs

---
+++ scripts/script.js
@@ -0,0 +1,40 @@
+const myLibrary = [ ];
+
+/*displayBook()
+
+function displayBook(){
+
+    myLibrary.forEach((element) => {
+        console.log(element)
+
+    })
+
+
+
+}
+
+function Book(title,author,year,pages){
+    //Constructor function
+    this.title = title;
+    this.auther = author;
+    this.year = year;
+    this.pages = pages;
+
+}
+*/
+
+//Dialog Form
+const showBtn = document.querySelector(".show-dialog")
+const dialog = document.querySelector(".dialog")
+const confirm = document.querySelector()
+
+showBtn.addEventListener("click", ()=> {
+    dialog.showModal();
+})
+
+
+
+function addBookToLibrary(){
+
+
+}
